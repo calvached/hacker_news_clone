50908d8b66de969f2238f2db97e50bea91da3925
thejessleigh <j.l.unrein@gmail.com>
2014-02-26 14:02:28 -0600

    Added login page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,20 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  user = User.find_by(username: params[:username],
+                      password_digest: params[:password])
+  session[:user_id] = user.id
+  redirect '/'
+end
+
+post '/signup' do
+  user = User.create(username: params[:username],
+                     password_digest: params[:password])
+  redirect '/login'
+end
