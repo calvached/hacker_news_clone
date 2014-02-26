3184c08d86ece2fecd22be3d888fdea35b404515
thejessleigh <j.l.unrein@gmail.com>
2014-02-26 14:24:44 -0600

    created validations for signup and login.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,14 +8,16 @@ get '/login' do
 end
 
 post '/login' do
-  user = User.find_by(username: params[:username],
-                      password_digest: params[:password])
-  session[:user_id] = user.id
-  redirect '/'
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  end
 end
 
 post '/signup' do
   user = User.create(username: params[:username],
-                     password_digest: params[:password])
+                     password: params[:password],
+                     password_confirmation: params[:password_confirmation])
   redirect '/login'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments, through: :posts
+
+  has_secure_password
+  validates :username, uniqueness: true, presence: true
   # Remember to create a migration!
 end
